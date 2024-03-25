976da6b1d48c559ec161d4442325aa145b0723a3
Allody <m.bogdanov2@g.nsu.ru>
2024-03-25 22:48:37 +0700

    1) Более детальный вывод ошибок и их логирование. 2) Добавление javadoc для методов сканирования. 3) Добавление комментариев внутри кода для лучшего понимания. 4) Исправление небольшой грамматической ошибки.

--- src/main/java/ru/nsu/exception/WrongJsonException.java
+++ src/main/java/ru/nsu/exception/WrongJsonException.java
@@ -1,13 +1,29 @@
 package ru.nsu.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+
 import java.io.Serial;
 
+/**
+ * Ошибка, связанная с информацией о бине в JSON конфигурации.
+ */
+@Slf4j
 public class WrongJsonException extends RuntimeException {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
-    public WrongJsonException(String beanName) {
-        super("Error with json" + beanName);
+    /**
+     * Выводим ошибку, связанную с информацией о бине в JSON конфигурации и
+     * записываем информацию о ней в файл для логов.
+     * @param beanName название бина для лучшего ориентира.
+     * @param description подробное описание ошибки.
+     */
+    public WrongJsonException(String beanName, String description) {
+        super("Error with json for bean " + beanName + description);
+        MDC.put("beanName", beanName);
+        log.error(description);
+        MDC.remove("beanName");
     }
 }
